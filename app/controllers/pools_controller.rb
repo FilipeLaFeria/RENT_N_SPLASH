12d1d5d3bcf42b88cd0cd5530c79a2e56a75b850
aura lopes <aura.lopes@gmail.com>
2021-06-01 10:47:50 +0100

    latest

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,2 +1,14 @@
 class PoolsController < ApplicationController
+  #before_action :set_pool, only: [:show]
+
+  def show
+    @pool = Pool.find(params[:id])
+  end
+
+  private
+
+  # def set_pool
+  #    @pool = Pool.find(params[:id])
+  # end
+
 end
