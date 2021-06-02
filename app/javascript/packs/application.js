37f59f5f3dfbc03f02f0a23d32d3678b6b931659
Jacome Saavedra <jacomebsaavedra@gmail.com>
2021-06-02 20:38:46 +0100

    Add logo and info on map markers, with button to direct to show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -38,4 +39,4 @@ document.addEventListener('turbolinks:load', () => {
   initMapbox();
   initFlatpickr();
   loadDynamicBannerText();
-})
+});
