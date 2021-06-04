fd5dd3cc46e9ad241337ccef06aec6942c88360a
Filipe La Feria <filipe.laferia@gmail.com>
2021-06-04 16:31:00 +0200

    booking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,14 @@ class PagesController < ApplicationController
 
   def dashboard
     @bookings = current_user.bookings
+    @markers = @bookings.map do |booking|
+        {
+          lat: booking.pool.latitude,
+          lng: booking.pool.longitude,
+          info_window: render_to_string(partial: "pools/info_window", locals: { pool: booking.pool }),
+          image_url: helpers.asset_url('logo.png')
+        }
+    end
   end
+
 end
