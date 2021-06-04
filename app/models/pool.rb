bbe5c00f53d69e20529f3dff9ca8795ee188e22d
Jacome Saavedra <jacomebsaavedra@gmail.com>
2021-06-04 13:00:38 +0100

    my pools page with sticky map and no margins and centered text on the buttons. Validations on the pool

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -2,6 +2,7 @@ class Pool < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   geocoded_by :address
+  validates :title, :address, :size, :description, :photo, presence: true
   after_validation :geocode, if: :will_save_change_to_address?
   include PgSearch::Model
   pg_search_scope :global_search,
